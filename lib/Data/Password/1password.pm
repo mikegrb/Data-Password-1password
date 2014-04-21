22aba56c640dfc53a4597b71cefd4bc545e6d8be
mikegrb <mgreb@linode.com>
2014-04-21 17:28:03 -0400

    some base module work finally \o/

--- lib/Data/Password/1password.pm
+++ lib/Data/Password/1password.pm
@@ -7,8 +7,34 @@ use Carp;
 use Data::Dumper;
 
 use Data::Password::1password::Types;
+use Data::Password::1password::Contents;
+use Data::Password::1password::Keys;
 
-has 'path'          => ( isa => 'ExistingPath', is => 'ro' );
+
+has 'path' => ( isa => 'ExistingPath', is => 'ro' );
+
+has 'contents' => (
+    isa     => 'Data::Password::1password::Contents',
+    is      => 'ro',
+    lazy    => 1,
+    builder => '_build_contents'
+);
+has 'keys' => (
+    isa     => 'Data::Password::1password::Keys',
+    is      => 'ro',
+    lazy    => 1,
+    builder => '_build_keys'
+);
+
+sub _build_contents {
+    my $self = shift;
+    return Data::Password::1password::Contents->new( path => $self->path );
+}
+
+sub _build_keys {
+    my $self = shift;
+    return Data::Password::1password::Keys->new( path => $self->path );
+}
 
 __PACKAGE__->meta->make_immutable;
 
