48daa010a2614706d8b534f5062b1021a1587c3d
mikegrb <mgreb@linode.com>
2014-04-22 22:55:59 -0400

    master_key attr for root object past root object on to Keys, Contents should probably get it too

--- lib/Data/Password/1password.pm
+++ lib/Data/Password/1password.pm
@@ -10,8 +10,9 @@ use Data::Password::1password::Types;
 use Data::Password::1password::Contents;
 use Data::Password::1password::Keys;
 
-
 has 'path' => ( isa => 'ExistingPath', is => 'ro' );
+
+has 'master_key' => ( isa => 'Str', is => 'ro', required => 1 );
 
 has 'contents' => (
     isa     => 'Data::Password::1password::Contents',
@@ -33,7 +34,10 @@ sub _build_contents {
 
 sub _build_keys {
     my $self = shift;
-    return Data::Password::1password::Keys->new( path => $self->path );
+    return Data::Password::1password::Keys->new(
+        path => $self->path,
+        root => $self
+    );
 }
 
 __PACKAGE__->meta->make_immutable;
