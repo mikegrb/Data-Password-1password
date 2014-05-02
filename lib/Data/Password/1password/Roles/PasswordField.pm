12a4bb4660c13093bb482026da14481eec96dbd9
mikegrb <mgreb@linode.com>
2014-05-01 22:10:59 -0400

    simplify PasswordField a bit use keyID rather than security level

--- lib/Data/Password/1password/Roles/PasswordField.pm
+++ lib/Data/Password/1password/Roles/PasswordField.pm
@@ -1,19 +1,18 @@
 package Data::Password::1password::Roles::PasswordField;
 
 use Moose::Role;
+
 use JSON::Any;
 use List::Util 'first';
 
 sub password {
     my $self = shift;
 
-    my $encrypted_data
-        = $self->root->keys->key_for_level( $self->securityLevel )
-        ->decrypt( $self->encrypted );
-    $encrypted_data = JSON::Any->jsonToObj($encrypted_data);
+    my $key  = $self->root->keys->get_key( $self->keyID );
+    my $data = JSON::Any->jsonToObj( $key->decrypt( $self->encrypted ) );
 
     my $password_field
-        = first { $_->{name} eq 'password' } @{ $encrypted_data->{fields} };
+        = first { $_->{name} eq 'password' } @{ $data->{fields} };
 
     return unless $password_field;
     return $password_field->{value};
